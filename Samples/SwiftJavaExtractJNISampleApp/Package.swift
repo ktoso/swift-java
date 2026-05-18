75e58685961904ae35891aba8aafb19c8532cdfe
Konrad Malawski <ktoso@apple.com>
2026-05-18 13:29:47 +0900

    build: Migrate SwiftJavaExtractJNISampleApp sample to Swift 6 language mode
    Drop .swiftLanguageMode(.v5) from MySwiftLibrary in
    SwiftJavaExtractJNISampleApp. (Sample's gradle/jextract step has a
    pre-existing failure unrelated to this change.)
    
    Part of incremental Swift 6 language mode migration.

--- Samples/SwiftJavaExtractJNISampleApp/Package.swift
+++ Samples/SwiftJavaExtractJNISampleApp/Package.swift
@@ -29,9 +29,6 @@ let package = Package(
       exclude: [
         "swift-java.config"
       ],
-      swiftSettings: [
-        .swiftLanguageMode(.v5)
-      ],
       plugins: [
         .plugin(name: "JExtractSwiftPlugin", package: "swift-java")
       ]
